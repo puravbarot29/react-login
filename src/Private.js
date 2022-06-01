997eb732762b6d3bfb45cdc7a16d68c7da8375fd
puravbarot29 <104783951+puravbarot29@users.noreply.github.com>
2022-06-01 11:56:11 -0400

    home.js and private.js

---
+++ src/Private.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Private(){
+return(
+    <div className="page">
+      <div className="warning">
+      <h1>Restricted Access</h1>
+      <h2>Authorized personnel only</h2>
+      </div>
+    </div>
+  );
+  
+}
+
+export default Private;
